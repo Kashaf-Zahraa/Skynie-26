59965f9ce9dcb34a1d52d1d4deec77469fb14cad
maheenkamran <maheenkamrann14@gmail.com>
2026-04-14 21:32:59 +0500

    Initialization

---
+++ app/src/main/java/com/example/skynie/fragments/MyTicketsFragment.java
@@ -0,0 +1,30 @@
+package com.example.skynie.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.skynie.R;
+
+public class MyTicketsFragment extends Fragment {
+
+    public MyTicketsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_tickets, container, false);
+    }
+}
